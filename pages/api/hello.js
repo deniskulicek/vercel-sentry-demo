8a1f690628ac1a07f424d68e0b8874f85c899465
Denis Kulicek <5512606+deniskulicek@users.noreply.github.com>
2021-05-14 21:26:03 +0200

    Add sentry to example api endpoint

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { withSentry } from '@sentry/nextjs';
 
-export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
+const doAsyncWork = () => Promise.reject(new Error('API Test 1'));
+doAsyncWork();
+
+async function handler(req, res) {
+  res.status(200).json({ name: 'John Doe' });
 }
+
+export default withSentry(handler);
